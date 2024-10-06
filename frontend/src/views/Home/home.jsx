0700d313eb4ad0c82074c1ed99984f583fad9427
PrathamGaidhane <prathamgaidhane123321@gmail.com>
2024-10-06 23:49:03 +0530

    change si=ome ui

--- frontend/src/views/Home/home.jsx
+++ frontend/src/views/Home/home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
 
   return (
     <>
-      <div>
+      <div className='homejs'>
         <Header />
         <Exploremenu category={category} setCategory={setCategory} />
         <FoodDisplay category={category} />
